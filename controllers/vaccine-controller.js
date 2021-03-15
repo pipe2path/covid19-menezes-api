200b7f1e01a5aa4990966d94254bbd5f17d26f0f
Kevin Menezes <kevinmenezes@yahoo.com>
2021-03-15 15:38:22 -0700

    added date parameter to stored proc

--- controllers/vaccine-controller.js
+++ controllers/vaccine-controller.js
@@ -2,7 +2,7 @@ var con = require('../common/database.js');
 
 exports.get_all_posts = function(req, res){
     res.setHeader('Access-Control-Allow-Origin','*');
-    let sql = "select u.userId, u.userName, c.cityId, name as cityName, content from Posts p" + 
+    let sql = "select u.userId, u.userName, c.cityId, name as cityName, content, dateCreated from Posts p" + 
             " inner join Users u on p.userId = u.userId" + 
             " inner join Cities c on c.cityId = u.cityId" ;
     con.query(sql, function (err, result) {
@@ -32,10 +32,14 @@ exports.get_posts_by_city = function(req, res){
 
 exports.post_data = function(req, res){
     res.setHeader('Access-Control-Allow-Origin','*');
+
+    var dateLocal = (new Date ((new Date((new Date(new Date())).toISOString() )).getTime() -
+        ((new Date()).getTimezoneOffset()*60000))).toISOString().slice(0, 19).replace('T', ' ');
+
     const name = req.body.name.replace(/'/g, "''");
     const city = req.body.city.replace(/'/g, "''");
     const comments = req.body.comments.replace(/'/g, "''");
-    let sql = "call createPost('" + name + "', '" + city + "', '" + comments + "')";
+    let sql = "call createPost('" + name + "', '" + city + "', '" + comments + "', '" + dateLocal + "')";
     con.query(sql, function (err, result) {
         if (err) throw err;
         res.send(result);
