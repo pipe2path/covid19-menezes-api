0033f58108e6bc5ff785412a672f6fd9ac4095e7
Kevin Menezes <kevinmenezes@yahoo.com>
2021-03-18 21:04:29 -0700

    added facility

--- controllers/vaccine-controller.js
+++ controllers/vaccine-controller.js
@@ -2,7 +2,7 @@ var con = require('../common/database.js');
 
 exports.get_all_posts = function(req, res){
     res.setHeader('Access-Control-Allow-Origin','*');
-    let sql = "select u.userId, u.userName, c.cityId, name as cityName, content, dateCreated from Posts p" + 
+    let sql = "select u.userId, u.userName, c.cityId, name as cityName, facility, content, dateCreated from Posts p" + 
             " inner join Users u on p.userId = u.userId" + 
             " inner join Cities c on c.cityId = u.cityId" +
             " order by dateCreated desc";
@@ -23,7 +23,7 @@ exports.get_all_cities = function(req, res){
 
 exports.get_posts_by_city = function(req, res){
     res.setHeader('Access-Control-Allow-Origin','*');
-    let sql = "select u.userId, u.userName, c.cityId, name as cityName, content, dateCreated from Posts p inner join Users u on u.userId = p.userId " + 
+    let sql = "select u.userId, u.userName, c.cityId, name as cityName, facility, content, dateCreated from Posts p inner join Users u on u.userId = p.userId " + 
             "inner join Cities c on u.cityId = c.cityId where c.cityId = " + req.params.cityId + " order by dateCreated desc" ;
     con.query(sql, function (err, result) {
         if (err) throw err;
@@ -40,13 +40,10 @@ exports.post_data = function(req, res){
     const name = req.body.name.replace(/'/g, "''");
     const city = req.body.city.replace(/'/g, "''");
     const comments = req.body.comments.replace(/'/g, "''");
-    let sql = "call createPost('" + name + "', '" + city + "', '" + comments + "', '" + dateLocal + "')";
+    const facility = req.body.facility.replace(/'/g, "''");
+    let sql = "call createPost('" + name + "', '" + city + "', '" + facility + "', '" + comments + "', '" + dateLocal + "')";
     con.query(sql, function (err, result) {
         if (err) throw err;
         res.send(result);
     });
 }
-
-// select u.userId, u.userName, c.cityId, name as cityName, content from Posts p
-//inner join Users u on p.userId = u.userId
-//inner join Cities c on c.cityId = u.cityId
